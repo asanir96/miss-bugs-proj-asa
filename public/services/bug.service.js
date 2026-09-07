1e3bd93a29c6b6f4639673ab598a804f21eec86c
asanir96 <asanir96@gmail.com>
2026-09-07 09:06:13 +0300

    Update bug service save function to use query parameters for saving bugs

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -47,11 +47,11 @@ function remove(bugId) {
 }
 
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+    const queryParams = `save?title=${bug.title}&severity=${bug.severity}`
+    if (bug._id) queryParams + `&id=${bug._id}`
+
+    return axios.get(BASE_URL + queryParams)
+        .then(res => res.data)
 }
 
 function _createBugs() {
